7d8b80551d04d9d0e7d4f9123f8b31c05eb67933
Calvin Roberts <roberts.calvin85@gmail.com>
2021-05-02 22:30:29 +0100

    Rotate arrow icon.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const selectField = document.getElementById('selectField'),
   selectText = document.getElementById('selectText'),
   options = document.getElementsByClassName('options'),
-  list = document.getElementById('list');
+  list = document.getElementById('list'),
+  arrowIcon = document.getElementById('arrowIcon');
 
 selectField.onclick = function () {
   list.classList.toggle('hide');
+  arrowIcon.classList.toggle('rotate');
 };
 
 for (option of options) {
   option.onclick = function () {
     selectText.innerHTML = this.textContent;
     list.classList.toggle('hide');
+    arrowIcon.classList.toggle('rotate');
   };
 }
