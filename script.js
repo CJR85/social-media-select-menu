059184f1e128dd81ef25f283a87616b9d1375bf5
Calvin Roberts <roberts.calvin85@gmail.com>
2021-05-02 22:25:10 +0100

    Add hide list function.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const selectField = document.getElementById('selectField'),
   selectText = document.getElementById('selectText'),
-  options = document.getElementsByClassName('options');
+  options = document.getElementsByClassName('options'),
+  list = document.getElementById('list');
+
+selectField.onclick = function () {
+  list.classList.toggle('hide');
+};
 
 for (option of options) {
   option.onclick = function () {
     selectText.innerHTML = this.textContent;
+    list.classList.toggle('hide');
   };
 }
